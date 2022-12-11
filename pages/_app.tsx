0a7c0cf6efe9b6210be2bfbd323ced97e1b2f92a
prince1096 <princeraj10121996@gmail.com>
2022-12-11 17:50:19 +0530

    css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,5 +3,9 @@ import "../styles/tailwind.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className="h-screen bg-gray-900">
+      <Component {...pageProps} />
+    </div>
+  );
 }
